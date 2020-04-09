db8d3ed088991ff491152ee5439e63e4b3fab293
Edward Bulliner <TauCritical@users.noreply.github.com>
2020-04-09 13:21:46 -0500

    change varname and remove response in makegrid

--- adcpkrig/make_grid.py
+++ adcpkrig/make_grid.py
@@ -1,20 +1,14 @@
+import numpy as np
+
 def makegrid(df):
 
-    x = np.array(TrimmedDF.POINT_X)
-    y = np.array(TrimmedDF.POINT_Y)
-    z = np.array(TrimmedDF.depth)
-
-
-    v_mag = np.array(TrimmedDF.v_mag)
-    v_emag = np.array(TrimmedDF.v_emag)
-    v_nmag = np.array(TrimmedDF.v_nmag)
-    v_hdir = np.array(TrimmedDF.v_hdir)
-
+    x = np.array(df.X)
+    y = np.array(df.Y)
+    z = np.array(df.Z)
 
     xdivisions = int((x.max()-x.min())/xspacing)+1
     ydivisions = int((y.max()-y.min())/yspacing)+1
     zdivisions = int((z.max()-z.min())/zspacing)+1
-
 
     xi = np.linspace(x.min(),x.max(),xdivisions)
     yi = np.linspace(y.min(),y.max(),ydivisions)
