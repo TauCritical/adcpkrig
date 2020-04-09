f88d73e40667b98519c23c64e96fb60d85433626
Edward Bulliner <TauCritical@users.noreply.github.com>
2020-04-09 14:41:02 -0500

    refactor grid_utils to class

--- adcpkrig/grid_utils.py
+++ adcpkrig/grid_utils.py
@@ -1,23 +1,22 @@
 import numpy as np
 
-def makegrid(df,xspacing,yspacing,zspacing):
+class grid():
+    def __init__(self,df,xspacing,yspacing,zspacing):
 
-    x = np.array(df.X)
-    y = np.array(df.Y)
-    z = np.array(df.Z)
+        x = np.array(df.X)
+        y = np.array(df.Y)
+        z = np.array(df.Z)
 
-    #xspacing = 5
-    #yspacing = 5
-    #zspacing = 0.5
+        #xspacing = 5
+        #yspacing = 5
+        #zspacing = 0.5
 
-    xdivisions = int((x.max()-x.min())/xspacing)+1
-    ydivisions = int((y.max()-y.min())/yspacing)+1
-    zdivisions = int((z.max()-z.min())/zspacing)+1
+        xdivisions = int((x.max()-x.min())/xspacing)+1
+        ydivisions = int((y.max()-y.min())/yspacing)+1
+        zdivisions = int((z.max()-z.min())/zspacing)+1
 
-    xi = np.linspace(x.min(),x.max(),xdivisions)
-    yi = np.linspace(y.min(),y.max(),ydivisions)
-    zi = np.linspace(z.min(),z.max(),zdivisions)
+        xi = np.linspace(x.min(),x.max(),xdivisions)
+        yi = np.linspace(y.min(),y.max(),ydivisions)
+        zi = np.linspace(z.min(),z.max(),zdivisions)
 
-    X, Y, Z = np.meshgrid(xi, yi, zi)
-
-    return (X,Y,Z)
+        self.X, self.Y, self.Z = np.meshgrid(xi, yi, zi)
