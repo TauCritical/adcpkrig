56d04fe37c4cd88f2f1256a3c343e12b074e7957
Edward Bulliner <TauCritical@users.noreply.github.com>
2020-04-09 14:50:43 -0500

    add method chunk to grid

--- adcpkrig/grid_utils.py
+++ adcpkrig/grid_utils.py
@@ -20,3 +20,27 @@ class grid():
         zi = np.linspace(z.min(),z.max(),zdivisions)
 
         self.X, self.Y, self.Z = np.meshgrid(xi, yi, zi)
+
+    def chunk(self,xrange,yrange,zrange):
+
+        xmin = xrange[0]
+        xmax = xrange[1]
+        ymin = yrange[0]
+        ymax = yrange[1]
+        zmin = zrange[0]
+        zmax = zrange[1]
+
+        Xchunk = self.X[ymin:ymax,xmin:xmax,zmin:zmax]
+        Ychunk = self.Y[ymin:ymax,xmin:xmax,zmin:zmax]
+        Zchunk = self.Z[ymin:ymax,xmin:xmax,zmin:zmax]
+
+        Xmax = Xchunk[0][-1][0]
+        Xmin = Xchunk[0][0][0]
+
+        Ymax = Ychunk[-1][0][0]
+        Ymin = Ychunk[0][0][0]
+
+        Zmax = Zchunk[0][0][-1]
+        Zmin = Zchunk[0][0][0]
+
+        return (Xchunk,Ychunk,Zchunk)
