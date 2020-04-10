d8394c30b04ab54684d297388e69b72b8b659bbe
Edward Bulliner <TauCritical@users.noreply.github.com>
2020-04-09 20:47:19 -0500

    create function makechunks

---
+++ adcpkrig/make_chunks.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+def makechunks(grid,lsize,lmargin,smallsize,smallmargin):
+
+    allshape = np.array(grid.X.shape)
+    largestdim = allshape.argmax()
+    secondlargestdim = allshape[-largestdim].argmax()
+
+    largechunksize = 20
+    largechunkmargin = 3
+    largechunkoverlap = largechunkmargin*2
+
+
+    smallchunksize = 5
+    smallchunkmargin = 2
+    smallchunkoverlap = smallchunkmargin*2
+
+    zchunksize = None
+
+    largechunks = int(allshape[largestdim]/(largechunksize-largechunkmargin))
+
+    smallchunks = int(allshape[secondlargestdim]/(smallchunksize-smallchunkmargin))
+
+    coords = []
+
+    for i in range(largechunks):
+        
+        largestart = max([0,i*(largechunksize-largechunkmargin)])
+        largeend = min([allshape[largestdim]-1,i*(largechunksize-largechunkmargin)+largechunksize])
+        
+        if largestart == 0:
+            largeend = 0+largechunksize
+        
+        if largeend == allshape[largestdim]-1:
+            largestart = allshape[largestdim]-1-largechunksize
+        
+        for j in range(smallchunks):
+            smallstart = max([0,j*(smallchunksize-smallchunkmargin)])
+            smallend = min([allshape[secondlargestdim]-1,j*(smallchunksize-smallchunkmargin)+smallchunksize])
+            
+            if smallstart == 0:
+                smallend = 0 + smallchunksize
+            
+            if smallend == allshape[secondlargestdim]-1:
+                smallstart = allshape[secondlargestdim]-1-smallchunksize
+            
+            coords.append([largestart,largeend,smallstart,smallend])
+    
+    return coords
