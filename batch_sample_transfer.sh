08a8040aab9f628afbbaaffd0f92ac0e3633aa21
Edward Bulliner <TauCritical@users.noreply.github.com>
2020-04-13 08:12:27 -0500

    create sample transfer batch file

---
+++ batch_sample_transfer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+
+aws s3 cp 2_coordsdict.pickle s3://adcpkrig/
+aws s3 cp 2_tests.pickle s3://adcpkrig/
+
+aws s3 cp 3_coordsdict.pickle s3://adcpkrig/
+aws s3 cp 3_tests.pickle s3://adcpkrig/
+aws s3 cp 4_coordsdict.pickle s3://adcpkrig/
+aws s3 cp 4_tests.pickle s3://adcpkrig/
+aws s3 cp 5_coordsdict.pickle s3://adcpkrig/
+aws s3 cp 5_tests.pickle s3://adcpkrig/
