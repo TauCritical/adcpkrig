d677d714734c883c785c57cac230b7a9b6630615
Edward Bulliner <TauCritical@users.noreply.github.com>
2020-04-13 21:23:31 -0500

    change batch order

--- batch_sample_train.sh
+++ batch_sample_train.sh
@@ -10,6 +10,24 @@ aws s3 cp 6_predicts_v_x.pickle s3://adcpkrig/
 aws s3 cp 6_predicts_v_y.pickle s3://adcpkrig/
 aws s3 cp 6_predicts_v_z.pickle s3://adcpkrig/
 
+python sample_train.py 10 10 2 20 2 v_mag
+python sample_train.py 10 10 2 20 2 v_x
+python sample_train.py 10 10 2 20 2 v_y
+python sample_train.py 10 10 2 20 2 v_z
+aws s3 cp 10_predicts_v_mag.pickle s3://adcpkrig/
+aws s3 cp 10_predicts_v_x.pickle s3://adcpkrig/
+aws s3 cp 10_predicts_v_y.pickle s3://adcpkrig/
+aws s3 cp 10_predicts_v_z.pickle s3://adcpkrig/
+
+python sample_train.py 11 10 8 20 8 v_mag
+python sample_train.py 11 10 8 20 8 v_x
+python sample_train.py 11 10 8 20 8 v_y
+python sample_train.py 11 10 8 20 8 v_z
+aws s3 cp 11_predicts_v_mag.pickle s3://adcpkrig/
+aws s3 cp 11_predicts_v_x.pickle s3://adcpkrig/
+aws s3 cp 11_predicts_v_y.pickle s3://adcpkrig/
+aws s3 cp 11_predicts_v_z.pickle s3://adcpkrig/
+
 python sample_train.py 7 40 4 10 4 v_mag
 python sample_train.py 7 40 4 10 4 v_x
 python sample_train.py 7 40 4 10 4 v_y
@@ -36,21 +54,3 @@ aws s3 cp 9_predicts_v_mag.pickle s3://adcpkrig/
 aws s3 cp 9_predicts_v_x.pickle s3://adcpkrig/
 aws s3 cp 9_predicts_v_y.pickle s3://adcpkrig/
 aws s3 cp 9_predicts_v_z.pickle s3://adcpkrig/
-
-python sample_train.py 10 10 2 20 2 v_mag
-python sample_train.py 10 10 2 20 2 v_x
-python sample_train.py 10 10 2 20 2 v_y
-python sample_train.py 10 10 2 20 2 v_z
-aws s3 cp 10_predicts_v_mag.pickle s3://adcpkrig/
-aws s3 cp 10_predicts_v_x.pickle s3://adcpkrig/
-aws s3 cp 10_predicts_v_y.pickle s3://adcpkrig/
-aws s3 cp 10_predicts_v_z.pickle s3://adcpkrig/
-
-python sample_train.py 11 10 8 20 8 v_mag
-python sample_train.py 11 10 8 20 8 v_x
-python sample_train.py 11 10 8 20 8 v_y
-python sample_train.py 11 10 8 20 8 v_z
-aws s3 cp 11_predicts_v_mag.pickle s3://adcpkrig/
-aws s3 cp 11_predicts_v_x.pickle s3://adcpkrig/
-aws s3 cp 11_predicts_v_y.pickle s3://adcpkrig/
-aws s3 cp 11_predicts_v_z.pickle s3://adcpkrig/
