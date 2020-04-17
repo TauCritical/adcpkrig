d5e44948d01800bcb423078a620ac034b803df12
Edward Bulliner <TauCritical@users.noreply.github.com>
2020-04-17 10:42:21 -0500

    update transfer script

--- batch_sample_transfer.sh
+++ batch_sample_transfer.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 #
 
-aws s3 cp 6_predicts_v_mag.pickle s3://adcpkrig/
-aws s3 cp 6_predicts_v_x.pickle s3://adcpkrig/
-aws s3 cp 6_predicts_v_y.pickle s3://adcpkrig/
-aws s3 cp 6_predicts_v_z.pickle s3://adcpkrig/
+aws s3 cp 7_predicts_v_mag.pickle s3://adcpkrig/
+aws s3 cp 7_predicts_v_x.pickle s3://adcpkrig/
+aws s3 cp 7_predicts_v_y.pickle s3://adcpkrig/
+aws s3 cp 7_predicts_v_z.pickle s3://adcpkrig/
+
+aws s3 cp 8_predicts_v_mag.pickle s3://adcpkrig/
+aws s3 cp 8_predicts_v_x.pickle s3://adcpkrig/
+aws s3 cp 8_predicts_v_y.pickle s3://adcpkrig/
+aws s3 cp 8_predicts_v_z.pickle s3://adcpkrig/
+
+aws s3 cp 9_predicts_v_mag.pickle s3://adcpkrig/
+aws s3 cp 9_predicts_v_x.pickle s3://adcpkrig/
+aws s3 cp 9_predicts_v_y.pickle s3://adcpkrig/
+aws s3 cp 9_predicts_v_z.pickle s3://adcpkrig/
+
+aws s3 cp 10_predicts_v_mag.pickle s3://adcpkrig/
+aws s3 cp 10_predicts_v_x.pickle s3://adcpkrig/
+aws s3 cp 10_predicts_v_y.pickle s3://adcpkrig/
+aws s3 cp 10_predicts_v_z.pickle s3://adcpkrig/
+
+aws s3 cp 11_predicts_v_mag.pickle s3://adcpkrig/
+aws s3 cp 11_predicts_v_x.pickle s3://adcpkrig/
+aws s3 cp 11_predicts_v_y.pickle s3://adcpkrig/
+aws s3 cp 11_predicts_v_z.pickle s3://adcpkrig/
